003bc9d411c30124ff91885d3c9cc68447a45734
Uli <sorjak1@gmail.com>
2024-10-17 14:10:37 -0700

    Adding easy configuration for urls

--- controllers/urls.js
+++ controllers/urls.js
@@ -1,5 +1,7 @@
-var express = require('express')
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
+var express = require('express')
 
 __app.set('view engine', 'ejs');  //tell Express we're using EJS
 __app.set('views', __dirname + '/../templates');  //set path to *.ejs files
@@ -13,7 +15,7 @@ __app.get('/', function(req, res){
     res.header("Pragma", "no-cache");
     res.header("Expires", 0);
     
-    res.render('index');
+    res.render('index', {player_url: process.env.SPACECHAT_PLAYER_URL});
 });
 
 __app.get('/map', function(req, res){
